a35e35831d07841090bc47c6e5187447a11da5f6
Esteban Valencia Caicedo <93455764+SoullessSapo@users.noreply.github.com>
2024-02-07 16:53:15 -0500

    add clases y avance factory

--- src/main/java/edu/eci/cvds/patterns/archetype/shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/patterns/archetype/shapes/ShapeFactory.java
@@ -1,6 +1,8 @@
 package edu.eci.cvds.patterns.archetype.shapes;
-import edu.eci.cvds.patterns.archetype.shapes.concrete;
-
+import edu.eci.cvds.patterns.archetype.shapes.concrete.Triangle;
+import edu.eci.cvds.patterns.archetype.shapes.concrete.Quadrilateral;
+import edu.eci.cvds.patterns.archetype.shapes.concrete.Pentagon;
+import edu.eci.cvds.patterns.archetype.shapes.concrete.Hexagon;
 public class ShapeFactory {
     public static Shape create(RegularShapeType type){
         switch (type){
