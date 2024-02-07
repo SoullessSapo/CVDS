e035b327aa7271ec3039b7050e8e30c07de41141
Esteban Valencia Caicedo <93455764+SoullessSapo@users.noreply.github.com>
2024-02-07 16:49:04 -0500

    add clases y avance factory

---
+++ src/main/java/edu/eci/cvds/patterns/archetype/shapes/RegularShapeType.java
@@ -0,0 +1,5 @@
+package edu.eci.cvds.patterns.archetype.shapes;
+
+public enum RegularShapeType {
+    Triangle, Quadrilateral, Pentagon, Hexagon
+}
---
+++ src/main/java/edu/eci/cvds/patterns/archetype/shapes/concrete/Pentagon.java
@@ -0,0 +1,8 @@
+package edu.eci.cvds.patterns.archetype.shapes.concrete;
+import edu.eci.cvds.patterns.archetype.shapes.Shape;
+
+public class Pentagon implements Shape {
+    public int getNumberOfEdges() {
+        return 5;
+    }
+}
